27050c4878a54726877ac663d9eeec8409f14ff7
leandro Villalba <107075681+VillalbaLeandro@users.noreply.github.com>
2024-04-05 10:41:48 -0300

    agregando funcionalidades de yarg

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,22 +1,6 @@
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true, //required
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        default: false
-    })
-    .check((argv, option) => {
-        if (isNaN(argv.b)) {
-            throw 'La base tiene que ser un numero'
-        }
-        return true;
-    })
-    .argv;
+const argv  = require('./config/yargs')
+
 console.clear();
 console.log('base: yarg', argv.b, 'listar:', argv.l);
 
