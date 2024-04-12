ee803c388fdb6783ee1129deb746d0f95b63bbdd
VillalbaLeandro <exemprary@gmail.com>
2024-04-12 18:07:05 -0300

    configuracion restserver

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 
 class Server {
     constructor() {
         this.app = express()
         this.port = process.env.PORT
+        this.usuariosPath = '/api/usuarios'
 
         // Middlewares
         this.middlewares();
@@ -16,36 +18,18 @@ class Server {
     }
 
     middlewares() {
+        // CORS 
+        this.app.use(cors())
+        
+        // Lectura y parseo del body
+        this.app.use(express.json());
+        
+        // Directorio publico
         this.app.use(express.static('public'));
     }
 
     routes() {
-        this.app.get('/api', (req, res) => {
-            res.json({
-                msg: 'get API'
-            })
-        })
-        this.app.put('/api', (req, res) => {
-            res.json({
-                msg: 'put API'
-            })
-        })
-        this.app.post('/api', (req, res) => {
-            res.json({
-                msg: 'post API'
-            })
-        })
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                msg: 'delete API'
-            })
-        })
-        this.app.patch('/api', (req, res) => {
-            res.json({
-                msg: 'patch API'
-            })
-        })
-
+        this.app.use(this.usuariosPath, require('../routes/usuarios'))
     }
 
     listen() {
